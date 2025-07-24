4bcbd0eaa0269e763472b9911395fcc5ec7bb826
sanazulfiqar002 <sanazulfiqar002@gmail.com>
2025-07-24 15:51:00 -0700

    Add initial Python learning file

---
+++ code_funda.py
@@ -0,0 +1,52 @@
+# Fundamental of python 
+print("Bismiallah")
+course="python programming"
+print(len(course))
+print(course[0])
+print(course[1])
+print(course[2])
+print(course[0:3])
+print(course[0:])
+print(course[0:-1])
+
+# Escape Sequence
+course= "python \\programming"
+course="python \"programming"
+course="python \n programming"
+print(course)
+
+# Formatted strings
+first = "Sana"
+last = "Zulfiqar"
+#full = (len(first + " " + last))
+full = (len) (first)
+print(full)
+
+# String Method
+course = "Programming language"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.encode())
+print(course.center(40))
+#print(course.strip())
+#print(course.lstrip())
+#print(course.rstrip())
+print(course.find("Pro"))
+print(course.replace("P","j"))
+print("Pro" in course)
+
+# Numbers
+x = 1
+x = 1.1
+x = 1+2j # a+bi
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10%3)
+
+# Working with Numbers
+
+
